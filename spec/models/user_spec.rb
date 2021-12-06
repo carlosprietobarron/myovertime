2a08afd9796abbf2e979e91744293a5701a310cd
Carlos Alberto Prieto Barrón <carlosprietobarron@outlook.com>
2021-12-06 14:21:25 -0600

    add rspec test for user table

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.create(email: "test@example.com", password: "123456", fisrt_name:"test1", last_name: "testlast")
+  end
+
+  it "create valid user" do
+    expect(@user).to be_valid
+  end
+
+  it "should have first name and last name" do
+    @user.fisrt_name = nil
+    @user.last_name = nil
+    expect(@user).not_to be_valid
+  end
 end
