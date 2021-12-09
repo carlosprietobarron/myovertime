75c762be6b1b1a3c2eb35b87c07c255ef6907a27
Carlos Alberto Prieto Barrón <carlosprietobarron@outlook.com>
2021-12-09 16:26:20 -0600

    add create new post and testing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,34 @@ class PostsController < ApplicationController
     def index
       @posts = Post.all
     end
+
+    def new
+      @post = Post.new
+    end
+  
+    def create
+      @post = Post.new(post_params)
+      
+      #@post.user_id = current_user.id
+  
+      if @post.save
+        puts "inside save if #{@post.inspect}"
+        redirect_to post_path(@post), notice: 'Your post was created successfully'
+      else
+        render :new
+      end
+    end
+
+    def show
+      @post = Post.find(params[:id])
+      puts "show post #{@post.inspect}"
+    end
+
+    def post_params
+	  	params.require(:post).permit(:date, :rationale)
+	  end
+
+	  def set_post
+	  	@post = Post.find(params[:id])
+	  end
 end
